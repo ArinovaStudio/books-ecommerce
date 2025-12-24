fe4e9784f63ca7c08f4d02e82333675992c0cc29
OM BHARGAV <ombhargav788@gmail.com>
2025-12-25 00:48:12 +0530

    solved merge conflicts

--- app/context/admin.tsx
+++ app/context/admin.tsx
@@ -37,7 +37,7 @@ export function AdminProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     async function fetchAdminRole() {
       try {
-        const res = await fetch("/api/auth/me", {
+        const res = await fetch("/api/test-admin-login", {
           credentials: "include",
         })
 
